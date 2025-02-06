8944c8c024fddb75652911b0c41b9ddf964af9e6
Carlos <5986905@alu.murciaeduca.es>
2025-02-06 15:29:42 +0100

    paginasText

---
+++ app/drawer/Historicos/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DetalleHistorico = () => {
+  return (
+    <View>
+      <Text>[id]</Text>
+    </View>
+  )
+}
+
+export default DetalleHistorico
