e1449cad40db374143f9c213955060762ed3e315
Carlos <5986905@alu.murciaeduca.es>
2025-02-06 12:19:35 +0100

    ColoresAdd

---
+++ themes/Colors.ts
@@ -0,0 +1,9 @@
+const Colors={
+    fondo:'#FDF9F9',
+    negro:'#1A1818',
+    azulOscuro:'#2B9EB3',
+    azulClaro:'#5BBED0',
+    celadonClaro:"#BCD9A0",
+    nyanza:"#FAFAD7",
+    celadonOscuro:"#85CC9C",
+ } as  const 
