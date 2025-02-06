6c2dfc091577b5ab3d3f5793158ca5cfad02b54d
Carlos <5986905@alu.murciaeduca.es>
2025-02-06 12:33:53 +0100

    AddEstructuraCarpetas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import {GestureHandlerRootView} from 'react-native-gesture-handler'
+import { Slot } from "expo-router";
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView>
+      <Slot />
+    </GestureHandlerRootView>
+  );
 }
