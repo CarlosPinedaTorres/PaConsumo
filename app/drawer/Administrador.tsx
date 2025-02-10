75e515a79e1f55fa1c9e4cf5b35567e2b66f8acc
Aurora Ramos Ferrer <1173752@alu.murciaeduca.es>
2025-02-10 20:39:26 +0100

    administrador

--- app/drawer/Administrador.tsx
+++ app/drawer/Administrador.tsx
@@ -8,11 +8,11 @@ const Administrador = () => {
   return (
     <View>
       <View style={[{backgroundColor:Colors.azulOscuro}, {width:400}, {height:200}, {alignSelf:'center'}, {flexDirection:'column'}, {borderRadius:20}, {top:'10%'}]}>
-      <Image source={require('../../assets/images/avatar.png')}  style={[{width:40}, {height:50}, {alignSelf:'center'}, {margin:10}]}/>
-            <TextInput style={[{color:Colors.negro},{height:34},{fontWeight:'bold'}, {margin:5},{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {width:200}, {fontSize:12}]} placeholder='Usuario'></TextInput>
+            <Image source={require('../../assets/images/avatar.png')}  style={[{width:40}, {height:50}, {alignSelf:'center'}, {margin:10}]}/>
+            <TextInput style={[{color:Colors.negro},{height:34},{fontWeight:'bold'}, {margin:5},{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {width:200}, {fontSize:12}]} placeholder='Nombre'></TextInput>
             <TextInput style={[{color:Colors.negro},{height:34},{fontWeight:'bold'}, {margin:5},{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {width:200}, {fontSize:12}]} placeholder='Contraseña' secureTextEntry></TextInput>
-            <Pressable style={[{backgroundColor:Colors.verde}, {borderRadius:10}, {height:30}, {alignSelf:'center'}]}>
-              <Text>Enviar</Text>
+            <Pressable style={[{backgroundColor:Colors.verde}, {borderRadius:10}, {height:30}, {alignSelf:'center'}, {padding:5}, {margin:4},{paddingHorizontal:10}, ]}>
+              <Text style={[{fontWeight:'bold'}]}>Enviar</Text>
             </Pressable>
       </View>
     </View>
