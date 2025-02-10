24b6c1b5a6f4c7082a375fec0f54e2fc920ba962
Carlos <5986905@alu.murciaeduca.es>
2025-02-10 20:22:40 +0100

    nuevo

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -5,34 +5,26 @@ import { Tabs } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 import Colors from '@/themes/Colors'
 
-
-
 const _layout = () => {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: Colors.negro  ,tabBarInactiveTintColor: Colors.azulOscuro, tabBarStyle: {backgroundColor: Colors.azulClaro} }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title:'Pagina Principal',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="film-outline" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="Historicos/index"
-        options={{
-          title: 'Historicos',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="construct-outline" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="Configuracion"
-        options={{
-          title: 'Configuracion',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="construct-outline" color={color} />,
-        }}
-      />
-      
-    </Tabs>
+    <Tabs screenOptions={{ tabBarActiveTintColor: Colors.negro, headerStyle:{backgroundColor: Colors.azulClaro}  ,tabBarInactiveTintColor: Colors.azulOscuro, tabBarStyle: {backgroundColor: Colors.azulClaro} }}>
+    <Tabs.Screen
+      name="index"
+      options={{
+        title:'Pagina Principal',
+        tabBarIcon: ({ color }) => <Ionicons size={28} name="film-outline" color={color} />,
+      }}
+    />
+ 
+    <Tabs.Screen
+      name="Configuracion"
+      options={{
+        title: 'Configuracion',
+        tabBarIcon: ({ color }) => <Ionicons size={28} name="construct-outline" color={color} />,
+      }}
+    />
+    
+  </Tabs>
 
    
   )
