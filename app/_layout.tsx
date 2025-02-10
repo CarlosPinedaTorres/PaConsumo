8bc70aac36fff7b03f9c2a903e518d5950bc3036
Carlos <5986905@alu.murciaeduca.es>
2025-02-10 19:49:39 +0100

    fuente

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
 import { Slot } from "expo-router";
 import React from "react";
-
+import {useFonts} from "expo-font"
 export default function RootLayout() {
+
+  const [loaded] = useFonts({
+    InterBold: require('../assets/fonts/InterBold.ttf'),
+  })
   return (
     <GestureHandlerRootView>
       <Slot />
