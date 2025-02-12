dbc8e5e0928a3fd33d29461320b509cecb0fc1a6
Aurora Ramos Ferrer <1173752@alu.murciaeduca.es>
2025-02-12 19:29:57 +0100

    holiwi

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,9 @@ export default function RootLayout() {
 
   const [loaded] = useFonts({
     InterBold: require('../assets/fonts/InterBold.ttf'),
+    InterRegular : require('../assets/fonts/Inter_18pt-Regular.ttf'),
+    InterBolder : require('../assets/fonts/Inter_18pt-Bold.ttf'),
+    InterExtraBolder : require('../assets/fonts/Inter_18pt-ExtraBold.ttf'),
   })
   return (
     <GestureHandlerRootView>
--- app/drawer/Administrador.tsx
+++ app/drawer/Administrador.tsx
@@ -9,10 +9,10 @@ const Administrador = () => {
     <View>
       <View style={[{backgroundColor:Colors.azulOscuro}, {width:400}, {height:200}, {alignSelf:'center'}, {flexDirection:'column'}, {borderRadius:20}, {top:'10%'}]}>
             <Image source={require('../../assets/images/avatar.png')}  style={[{width:40}, {height:50}, {alignSelf:'center'}, {margin:10}]}/>
-            <TextInput style={[{color:Colors.negro},{height:34},{fontWeight:'bold'}, {margin:5},{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {width:200}, {fontSize:12}]} placeholder='Nombre'></TextInput>
-            <TextInput style={[{color:Colors.negro},{height:34},{fontWeight:'bold'}, {margin:5},{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {width:200}, {fontSize:12}]} placeholder='Contraseña' secureTextEntry></TextInput>
+            <TextInput style={[{color:Colors.negro},{fontFamily:"InterExtraBold"},{height:34}, {margin:5},{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {width:200}, {fontSize:12}]} placeholder='Nombre'></TextInput>
+            <TextInput style={[{color:Colors.negro},{height:34},{fontFamily:"InterExtraBold"}, ,{margin:5},{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {width:200}, {fontSize:12}]} placeholder='Contraseña' secureTextEntry></TextInput>
             <Pressable style={[{backgroundColor:Colors.verde}, {borderRadius:10}, {height:30}, {alignSelf:'center'}, {padding:5}, {margin:4},{paddingHorizontal:10}, ]}>
-              <Text style={[{fontWeight:'bold'}]}>Enviar</Text>
+              <Text style={[{fontWeight:'bold'},{fontFamily:"InterExtraBold"}]}>Entrar</Text>
             </Pressable>
       </View>
     </View>
