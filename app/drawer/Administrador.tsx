8608a7c5bee3d828d6fed223cd895601441d89af
Manuel <7464032@alu.murciaeduca.es>
2025-02-14 17:42:13 +0100

    tabbbs2

--- app/drawer/Administrador.tsx
+++ app/drawer/Administrador.tsx
@@ -7,7 +7,7 @@ import {  View, Image} from "react-native";
 const Administrador = () => {
   return (
     <View>
-      <View style={[{backgroundColor:Colors.azulOscuro}, {width:1000}, {height:520}, {alignSelf:'center'}, {flexDirection:'column'}, {borderRadius:20}, {top:'10%'}]}>
+      <View style={[{backgroundColor:Colors.azulOscuro}, {width:1000}, {height:510}, {alignSelf:'center'}, {flexDirection:'column'}, {borderRadius:20}, {top:'10%'}]}>
             <Image source={require('../../assets/images/avatar.png')}  style={[{width:170}, {height:200}, {alignSelf:'center'}, {margin:20}]}/>
             <TextInput style={[{color:Colors.negro},{fontFamily:"InterExtraBold"},{height:74},{width:700}, {margin:1},{paddingLeft:15} ,{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {fontSize:40}]} placeholder='Nombre'></TextInput>
             <TextInput style={[{color:Colors.negro},{height:74},{fontFamily:"InterExtraBold"}, {height:74},{width:700}, {paddingLeft:15} ,{margin:10},{alignSelf:'center'}, {backgroundColor:Colors.fondo}, {borderRadius:10}, {fontSize:40}]} placeholder='Contraseña' secureTextEntry></TextInput>
